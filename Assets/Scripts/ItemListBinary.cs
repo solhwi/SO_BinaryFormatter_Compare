7e10b6589e881d55d79c37e3594456f783694887
solhwi park <67333432+solhwi@users.noreply.github.com>
2021-12-20 14:08:56 +0900

    Update ItemListBinary.cs

--- Assets/Scripts/ItemListBinary.cs
+++ Assets/Scripts/ItemListBinary.cs
@@ -6,24 +6,33 @@ using System.IO;
 using Newtonsoft.Json;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
+using System.Text;
 
 [CreateAssetMenu(fileName = "ItemListBinary", menuName = "SO/ItemListBinary")]
-public class ItemListBinary : ScriptParser
+public class ItemListBinarySO : ScriptParser
 {
     public ItemList itemList;
 
     public ItemList.TempInfo GetInfo(int idx) => itemList.GetInfo(idx);
     public bool Init()
     {
-        ItemList list = Parser<ItemList>("Datas/ItemList");
-
         BinaryFormatter formatter = new BinaryFormatter();
-        FileStream fs = new FileStream($"{Application.dataPath}/AssetBundles/Binary/ItemListBinary.txt", FileMode.Create);
-        formatter.Serialize(fs, list);
+        
+        string data = File.ReadAllText($"{Application.dataPath}/Resources/Datas/ItemList.json", Encoding.UTF8);
+        byte[] binaryData = Encoding.UTF8.GetBytes(data);
 
-        fs.Close();
+        using (FileStream fs = new FileStream($"{Application.dataPath}/AssetBundles/Binary/ItemListBinary.txt", FileMode.Create))
+        {
+            formatter.Serialize(fs, binaryData);
+            fs.Close();
+        }
 
-        return !list.IsVaild;
+        return binaryData.Length > 0;
+    }
+
+    public void MakeItemList(string data)
+    {
+        Debug.Log(data.Length);     
     }
 
 }
