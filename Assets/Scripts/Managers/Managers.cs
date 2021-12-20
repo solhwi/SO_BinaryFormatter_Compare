b3a5f5c44b91daf71366675b7f7a50bb858c7492
solhwi park <67333432+solhwi@users.noreply.github.com>
2021-12-20 14:09:25 +0900

    Update Managers.cs

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -12,7 +12,7 @@ public class Managers : Singleton<Managers>
     {
         get 
         {
-            return Instance.scripts; // instance가 존재한다면 scripts를 반환, 그게 아니라면 초기화 후 반환합니다.
+            return Instance.scripts;
         } 
     }
 
@@ -20,27 +20,8 @@ public class Managers : Singleton<Managers>
     {
         get
         {
-            if(Instance.resources.bundle == null)
-            {
-                 
-            }
-
             return Instance.resources;
         }
     }
-
-    IEnumerator Start()
-    {
-        UnityWebRequest www = UnityWebRequestAssetBundle.GetAssetBundle("C:/ScriptParse/Assets/AssetBundles/manifest");
-        yield return www.SendWebRequest();
-
-        if (www.result != UnityWebRequest.Result.Success)
-        {
-            Debug.Log(www.error);
-        }
-        else
-        {
-            resources.bundle = DownloadHandlerAssetBundle.GetContent(www);
-        }
-    }
 }
+
